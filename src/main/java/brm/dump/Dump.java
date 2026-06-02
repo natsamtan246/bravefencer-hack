cbcb2710e0829a029c1674b86a52895acf06e774
natsamtan246 <harjamnat@yahoo.com>
2026-06-02 05:56:18 -0500

    pointer vibecode?

--- src/main/java/brm/dump/Dump.java
+++ src/main/java/brm/dump/Dump.java
@@ -32,9 +32,9 @@ public class Dump {
 			english=new AllScriptsDumper(splitdir,"en").toHashMap();
 		}
 		splitdir= Conf.desktop+"brmjp/";
-		new AllScriptsDumper(splitdir,"jp");
+		new AllScriptsDumper(splitdir,"jp")
 //		.print();
-//		.exportJpWithEnglish(excelFile, excel, english);
+		.exportJpWithEnglish(excelFile, excel, english);
 //		.exportByScript(Conf.desktop+"brm-jp.xlsx");//do not remove duplicate sentence, shown by per script, used to compare jp script with english script
 		
 		new MenuDumper().dumpAllMenu(excel, splitdir);
