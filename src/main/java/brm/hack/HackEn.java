e7b8387e8fdd363ae1c0b28c35d6a46d9b87427f
natsamtan246 <harjamnat@yahoo.com>
2026-06-03 11:02:17 -0500

    pointer vibecode?

--- src/main/java/brm/hack/HackEn.java
+++ src/main/java/brm/hack/HackEn.java
@@ -6,33 +6,24 @@ import brm.Conf;
 import brm.ScriptConfigLoader;
 
 public class HackEn {
-
     public static void main(String[] args) throws Exception {
         String splitdir = Conf.desktop + "brmen/";
         File excel = new File(Conf.desktop + "brm-en.xlsx");
-
-        ScriptConfigLoader scriptConfig =
-                new ScriptConfigLoader("en", splitdir);
-
+        ScriptConfigLoader scriptConfig = new ScriptConfigLoader("en", splitdir);
         Encoding enc = new EncodingEn();
 
-        new MainImporterEn()
-                .importFrom(excel, splitdir, scriptConfig.main, enc);
+        new MainImporterEn().importFrom(excel, splitdir, scriptConfig.main, enc);
 
-        /*
-         * Leave SCRIPTS disabled for the first boot test.
-         */
-        // new AllScriptsImporterEn()
-        //         .importFrom(excel, splitdir, enc);
+        new AllScriptsImporterEn().importFrom(excel, splitdir, enc);
 
         ErrMsg.checkErr();
 
         /*
-         * For now, rebuild only MAIN.CD.
-         * Do not recompress all SCxx archives yet.
+         * For first SCRIPTS test, rebuild only SC01.
+         * Do not full rebuild all SCxx yet.
          */
-        CdRebuilder.rebuildOne(splitdir, Conf.outdir, "MAIN");
+        CdRebuilder.rebuildOne(splitdir, Conf.outdir, "SC01");
 
-        System.out.println("English MAIN import/rebuild complete.");
+        System.out.println("English SCRIPTS import/rebuild complete.");
     }
 }
